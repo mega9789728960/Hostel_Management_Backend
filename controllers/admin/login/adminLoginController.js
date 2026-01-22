ad75c702403b814b9d2076bd4b011d8fcd113554
mega9789728960 <benmega500@gmail.com>
2026-01-22 18:43:28 +0530

    Gtgv

--- controllers/admin/login/adminLoginController.js
+++ controllers/admin/login/adminLoginController.js
@@ -51,9 +51,7 @@ export async function adminLoginController(req, res) {
     // Save refresh token
     await pool.query(
       `INSERT INTO refreshtokens (user_id, tokens, expires_at, role)
-       VALUES ($1, $2, NOW() + interval '${process.env.REFRESH_TOKEN_LIFE[0]} days', 'admin')
-       ON CONFLICT (user_id, role)
-       DO UPDATE SET tokens = EXCLUDED.tokens, expires_at = EXCLUDED.expires_at`,
+       VALUES ($1, $2, NOW() + interval '${process.env.REFRESH_TOKEN_LIFE[0]} days', 'admin')`,
       [user.id, refreshToken]
     );
 
